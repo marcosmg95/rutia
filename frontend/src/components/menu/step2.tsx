bd0b49376261da77682c8ee9b8b5926a40ff66f7
abrilser <abril.serrano.espindola@gmail.com>
2024-11-09 06:30:41 +0100

    Step 2 added second request and data handling WIP

--- frontend/src/components/menu/step2.tsx
+++ frontend/src/components/menu/step2.tsx
@@ -1,8 +1,50 @@
+import { Coordenades, Marcador, ResultatAPI } from "@/context/domain";
 import { useMenuContext } from "@/context/menuContext";
 
 export default function Step2() {
-  const { nextStep } = useMenuContext();
+  const { data, setData, nextStep, setMarkers, firstResults } = useMenuContext();
 
+  const getLastMarkers = async () => {
+    const url = `${process.env.NEXT_PUBLIC_API_URL || ''}customization`
+    const body = {
+      context: data.context,
+      locations: firstResults,
+    };
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json',
+        'ngrok-skip-browser-warning': 'true',
+      }
+    }).then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error("Something went wrong on API server!");
+      }
+    }).then((response) => {
+      console.debug('debug', response);
+
+      // const marcadors: Marcador[] = response.map((r: ResultatAPI) => {
+      //   return {
+      //     nom: r.title,
+      //     localitzacio: {
+      //       lat: r.location?.latitude,
+      //       lng: r.location?.longitude,
+      //     }
+      //   }
+      // })
+      // setMarkers(marcadors);
+    }).catch((error) => {
+      console.error('error', error);
+    });
+  };
+
+  const clickGenerar = () => {
+    getLastMarkers();
+    nextStep();
+  }
 
   return (
     <>
@@ -12,9 +54,16 @@ export default function Step2() {
           rows={6}
           className="w-full"
           placeholder=""
+          value={data.context}
+          onChange={(e) => setData({ ...data, context: e.target.value })}
         />
       </div>
-      <button className="primary uppercase px-7 self-center mt-auto text-lg" onClick={nextStep}>Generar ruta</button>
+      <button
+        className="primary uppercase px-7 self-center mt-auto text-lg"
+        onClick={clickGenerar}
+      >
+        Generar ruta
+      </button>
     </>
   )
 }
